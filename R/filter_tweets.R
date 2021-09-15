bdafd305847e8ece15f22591333704409aacc5f7
=?UTF-8?q?Andreas=20Buchm=C3=BCller?= <42041832+abuchmueller@users.noreply.github.com>
2021-09-15 19:25:40 +0200

    added filtering functionality for iterative filtering close #8

---
+++ R/filter_tweets.R
@@ -0,0 +1,58 @@
+#' Filter tweets
+#' @description Filter tweets by keywords.
+#' @usage filter_tweets(data, keywords, include = TRUE)
+#' @details Use this function if you want your Tweets to contain certain keywords.
+#' This can be used for iterative filtering to create more coherent topic models.
+#' Keyword filtering is always case insensitive (lowercase).
+#' @param keywords Character string of keywords for black- or whitelisting provided via a comma separated character string.
+#' @param include Logical. Indicate where to perform exclusive or inclusive filtering.
+#' Inclusive filtering is akin to whitelisting keywords. Exclusive filtering is blacklisting certain keywords.
+#' @inheritParams pool_tweets
+#' @return Data frame of Tweets containing specified keywords
+#'
+#' @export
+#' @examples
+#'
+#' \dontrun{
+#'
+#' library(TweetLocViz)
+#'
+#' # Exclude Tweets that mention "football" and/or "mood"
+#' mytweets <- load_tweets("inst/extdata/tweets_20191027-141233.json")
+#' keyword_dict <- "football,mood"
+#' mytweets_reduced <- filter_tweets(mytweets, keywords = keyword_dict, include = FALSE)
+#' }
+
+
+filter_tweets <- function(data, keywords, include = TRUE) {
+
+  # Turn keywords dictionary into Regex
+  dict <- stringr::str_replace_all(keywords, ",", "|")
+
+  if (include) {
+
+    # subset based on Regex
+    included <- which(stringr::str_detect(tolower(data$text), dict))
+
+    # return subset rows
+    df_new <- data[included, ]
+
+    return(df_new)
+
+  }
+
+  if (!include) {
+
+    # subset based on Regex
+    excluded <- which(!stringr::str_detect(tolower(data$text), dict))
+
+    # return subset rows
+    df_new <- data[excluded, ]
+
+    return(df_new)
+
+  }
+
+}
+
+
