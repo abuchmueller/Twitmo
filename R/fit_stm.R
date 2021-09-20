3cd63b68c032b16a64e397b1ad6ba798df14eaf1
=?UTF-8?q?Andreas=20Buchm=C3=BCller?= <42041832+abuchmueller@users.noreply.github.com>
2021-09-20 18:33:36 +0200

    add stm modeling support close #3

--- R/fit_stm.R
+++ R/fit_stm.R
@@ -1,38 +1,119 @@
-#' Fit STM Topic Model
-#' @description Estimate a STM topic model
-#' @usage fit_stm(pooled_dfm, n_topics = 2L, meta = NULL, ...)
-#' @param pooled_dfm Object of class dfm (see \link[quanteda]{dfm}) containing (pooled) Tweets.
-#' @param n_topics Integer with number of topics
-#' @param meta Formulaa object specifying external covariates (meta data) to use.e.g. \code{~favourites_count + retweet_count}
-#' By default uses favorite count, retweet count, emojis and hashtags from Tweets.
-#' @param ... Additional arguments passed to \link[stm:stm]{stm}.
-#' @return Object of class \link[stm:stm]{STM}
+#' Fit STM (Structural topic model)
+#' @description Estimate a structural topic model
+#' @details Use this to function estimate a STM from a data frame of parsed Tweets.
+#' Works with unpooled Tweets only. Pre-processing and fitting is done in one run.
+#' @param n_topics Integer with number of topics.
+#' @param xcov Either a \[stats]{formula} with an empty left-hand side specifying external covariates
+#' (meta data) to use.e.g. \code{~favourites_count + retweet_count}
+#' or a character vector (\code{c("favourites_count", "retweet_count")})
+#' or comma seperated character string (\code{"favourites_count,retweet_count"})
+#' with column names implying which metadata to use as external covariates.
+#' @param remove_punct Logical. Indicates wheter punctuation (includes Twitter hashtags and usernames)
+#' should be removed. Defaults to TRUE.
+#' @param stem Logical. If \code{TRUE} turn on word stemming for terms.
+#' @param ... Additional arguments passed to \link[stm]{stm}.
+#' @return Object of class \link[stm]{stm}.
+#' @inheritParams pool_tweets
 #'
 #' @export
 #' @seealso \link[stm]{stm}
 
-fit_stm <- function(pooled_dfm, n_topics = 2L, meta = NULL, ...) {
+fit_stm <- function(data, n_topics = 2L, xcov,
+                    remove_punct = TRUE,
+                    stem = TRUE,
+                    remove_url = TRUE,
+                    remove_emojis = TRUE,
+                    stopwords = "en",
+                    ...) {
 
-  dfm2stm <- quanteda::convert(pooled_dfm, to = "stm")
+  if (missing(data)) stop("Missing data frame with parsed tweets")
 
-  model.stm <- stm::stm(dfm2stm$documents,
-                    dfm2stm$vocab,
-                    K = n_topics,
-                    data = dfm2stm$meta,
-                    max.em.its = 75,
-                    ...)
+  if (missing(xcov)) stop("Please provide at least one external covariate for STMs
+If you wish to fit a model without external covariates use `fit_ctm()`.
+Type `?fit_stm` to learn more.")
 
+  n_topics <- as.integer(n_topics)
+
+  # if stopwords are missing or not a character vector no stopwords will be used
+  if (!is.character(stopwords)|missing(stopwords)) stopwords <- FALSE
+
+  stopifnot(is.logical(stem),
+            is.logical(remove_punct),
+            is.logical(remove_url),
+            is.logical(remove_emojis),
+            is.integer(n_topics))
+
+  # perform twitter specific text cleaning by removing urls, emojis, hashtags and usernames from text
+  # add single-point latitude and longitude variables to tweets data
+  data <- rtweet::lat_lng(data)
+
+  # remove duplicates quoted tweets and retweets
+  data <- data[data$is_quote == "FALSE" & data$is_retweet == FALSE, ]
+
+  # remove emojis from tweets
+  if (remove_emojis) {
+    data$text <- stringr::str_remove_all(data$text, emojis_regex)
+  }
+
+  # remove urls
+  if (remove_url) {
+    data$text <- stringr::str_replace_all(data$text, "https://t.co/[a-z,A-Z,0-9]*","")
+  }
+
+  # remove hashtags / usernames
+  if (remove_punct) {
+    data$text <- stringr::str_replace_all(data$text,"#[a-z,A-Z,_]*","")
+
+    # remove references to usernames
+    data$text <- stringr::str_replace_all(data$text,"@[a-z,A-Z,_]*","")
+
+  }
+
+  # perform usual pre-processing steps
+  processed <- stm::textProcessor(data$text,
+                                  metadata = data,
+                                  lowercase = TRUE,
+                                  removestopwords = TRUE,
+                                  removepunctuation = remove_punct,
+                                  stem = stem,
+                                  language = stopwords,
+                                  customstopwords = c("amp", "na", "rt", "via"))
+  # prepare data for stm modeling
+  out <- stm::prepDocuments(documents = processed$documents,
+                            vocab = processed$vocab,
+                            meta = processed$meta)
+
+  # routine for metadata formulation
+  # stm takes a formula object with the lhs empty as input
+  # if user inputs character string for metadata
+  if (is.character(xcov) & length(xcov) == 1) {
+    xcov <- paste("~", paste(unlist(strsplit(xcov, ",")), collapse = "+"), collapse = "", sep = "")
+    xcov <- stats::as.formula(xcov)
+  }
+  # if user inputs character vector for metadata
+  if (is.character(xcov) & length(xcov) > 1) {
+    xcov <- paste("~", paste(xcov, collapse = "+"), collapse = "", sep = "")
+    xcov <- stats::as.formula(xcov)
+  }
+  # make sure external covariates are a into a formula object
+  # if user passes formula obj
+  if (is.language(xcov)) xcov <- stats::as.formula(xcov)
+  model.stm <- stm::stm(documents = out$documents, vocab = out$vocab, data = out$meta,
+                        K = 7,
+                        prevalence = xcov,
+                        max.em.its = 75,
+                        init.type = "Spectral",
+                        ...)
   return(model.stm)
 
 }
 
-#' Fit CTM Topic Model
+#' Fit CTM (Correlated topic model)
 #' @description Estimate a CTM topic model.
-#' @usage fit_ctm(pooled_dfm, n_topics = 2L, ...)
 #' @param pooled_dfm Object of class dfm (see \link[quanteda]{dfm}) containing (pooled) Tweets.
 #' @param n_topics Integer with number of topics
-#' @param ... Additional arguments passed to \link[stm:stm]{stm}.
-#' @return Object of class \link[stm:stm]{STM}
+#' @param ... Additional arguments passed to \link[stm]{stm}.
+#' @return Object of class \link[stm]{stm}
 #'
 #' @export
 #' @seealso \link[stm]{stm}
@@ -53,9 +134,8 @@ fit_ctm <- function(pooled_dfm, n_topics = 2L, ...) {
 }
 
 
-#' Find best STM/CTM model
-#' @description Gridsearch for optimal K for your STM/CTM model.
-#' @usage find_stm(pooled_dfm, search_space = seq(4, 20, by = 2), ...)
+#' Find best STM/CTM
+#' @description Gridsearch for optimal K for your STM/CTM
 #' @param pooled_dfm object of class dfm (see \link[quanteda]{dfm}) containing (pooled) tweets
 #' @param search_space Vector with number of topics to compare different models.
 #' @param ... Additional parameters passed to \link[stm]{searchK}
@@ -75,32 +155,3 @@ find_stm <- function(pooled_dfm, search_space = seq(4, 20, by = 2), ...) {
 
 }
 
-
-
-prepare_stm <- function(data,
-                        lowercase = TRUE,
-                        removestopwords = TRUE,
-                        remove_punct = TRUE,
-                        stem = TRUE,
-                        language = "en") {
-
-  # Helper function for preparing data to pass to stm::stm
-
-  processed <- stm::textProcessor(data$text,
-                                  metadata = data,
-                                  lowercase = lowercase,
-                                  removestopwords = removestopwords,
-                                  removepunctuation = remove_punct,
-                                  stem = stem,
-                                  language = language,
-                                  customstopwords = c("amp", "na", "rt", "via"))
-
-
-  out <- stm::prepDocuments(documents = processed$documents,
-                            vocab = processed$vocab,
-                            meta = processed$meta)
-
-
-  return(out)
-
-}
